ca787f7daed1818202211c223a8b59927fe39436
zhshakuali <zhansayashakuali200309@gmail.com>
2023-05-10 01:21:33 +0600

    add all MVC files

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        update()
     }
-
-
+    
+    
+    
+   
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+       
+        storyBrain.nextStory(userChoice: userAnswer)
+       
+        update()
+        
+        
+    }
+    
+    
+    func update(){
+        let firstChoices = storyBrain.usersFirstChoice()
+        let secondChoice = storyBrain.usersSecondChoice()
+        choice1Button.setTitle(firstChoices, for: .normal)
+        choice2Button.setTitle(secondChoice, for: .normal)
+        
+        storyLabel.text = storyBrain.getStoryText()
+        
+    }
+    
 }
 
+ 
---
+++ Destini-iOS13/StoriesDB.swift
@@ -0,0 +1,11 @@
+//
+//  StoriesDB.swift
+//  Destini-iOS13
+//
+//  Created by Жансая Шакуали on 28.04.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
